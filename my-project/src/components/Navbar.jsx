db0843035da51356c739b5ff3ed2fcd6d1eca0f5
NadeemSilawat <im.nadeem.silawat@gmail.com>
2025-08-13 12:31:06 +0530

    Wellness Club

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 function Navbar() {
   return (
     <div className=" w-full z-50 flex items-center justify-between bg-gradient-to-r from-green-200 via-green-700 to-green-600 shadow-lg px-6 py-2">
-      {/* Left: Wellness Club logo and text */}
+      {/* Wellness logo */}
       <div className="flex items-center gap-2 m-3">
         <img
           src="src/assets/20250615_191433_0000.png"
           alt="Wellness Club Logo"
           className="h-14 w-auto object-contain drop-shadow-lg"
         />
-        <span className="text-2xl font-bold text-white tracking-wide drop-shadow">Wellness Club</span>
-      </div>  
-      {/* Right: JIET Logo */}
+        <span className="text-2xl font-bold text-white tracking-wide drop-shadow">
+          Wellness Club
+        </span>
+      </div>
+      {/* JIET logo */}
       <div className="flex items-center">
         <img
-          src="src/assets/react.svg"
+          src="src/assets/JIET+Universe+Logo.png"
           alt="JIET Logo"
           className="h-10 w-auto object-contain drop-shadow-lg"
         />
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
